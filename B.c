8708061d8447ffb3444993f983c211c4b338067b
Aniruddha6065 <145911846+Aniruddha6065@users.noreply.github.com>
2024-04-13 12:06:10 +0530

    Update B.c

--- B.c
+++ B.c
@@ -3,7 +3,7 @@ int main()
 {
     int s=0,n,l=9,m=0,pos=0,f=0;
     int ar[]={10,20,30,40,50,55,60,65,70,90};
-    printf("Enter number to serch:");
+    printf("Enter number to search:");
     scanf("%d",&n);
     while(s<=l)
     {
